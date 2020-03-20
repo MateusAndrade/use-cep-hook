2fa0f7d413126663d1cc154e327a2acb55b520aa
MateusAndrade <mateus.andrade47@outlook.com>
2020-03-20 13:15:15 -0300

    feat: adding basic hook and config

---
+++ index.tsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect, useMemo, useCallback } from "react";
+
+const INITIAL_CEP = {
+  cep: "",
+  logradouro: "",
+  complemento: "",
+  bairro: "",
+  localidade: "",
+  uf: "",
+  unidade: "",
+  ibge: "",
+  gia: ""
+};
+
+const getViaCep = cep => `https://viacep.com.br/ws/${cep}/json`;
+
+const useViaCep = search => {
+  const cleanCep = useMemo(() => search && search.replace(/\D+/g, ""), [
+    search
+  ]);
+
+  const [loading, setLoading] = useState(false);
+
+  const [cep, setCep] = useState(INITIAL_CEP);
+
+  const [error, setError] = useState(null);
+
+  const searchCep = useCallback(async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(getViaCep(cleanCep));
+
+      const responseJson = await response.json();
+
+      setCep(responseJson);
+
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+      setCep(INITIAL_CEP);
+    }
+  }, [cleanCep]);
+
+  useEffect(() => {
+    if (cleanCep.length === 8) {
+      searchCep();
+    }
+  }, [cleanCep, searchCep]);
+
+  return [loading, cep, error];
+};
+
+export default useViaCep;
