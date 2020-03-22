2e11b961da750a6fafb09bfae99e2f267a659e21
MateusAndrade <mateus.andrade47@outlook.com>
2020-03-22 19:15:27 -0300

    feat: adding basic returns with TS

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,17 @@
-export type Cep = {
-  cep: string;
-  logradouro: string;
-  complemento: string;
-  bairro: string;
-  localidade: string;
-  uf: string;
-  unidade: string;
-  ibge: string;
-  gia: string;
+declare module "use-via-cep" {
+  export type Cep = {
+    cep: string;
+    logradouro: string;
+    complemento: string;
+    bairro: string;
+    localidade: string;
+    uf: string;
+    unidade: string;
+    ibge: string;
+    gia: string;
+  };
+
+  export type HookReturn = [boolean, Cep, unknown];
+
+  export function useViaCep(postalCode: string): HookReturn;
 }
