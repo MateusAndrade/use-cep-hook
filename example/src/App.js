537942e66b044cc99c385b0588522a29bf81520d
MateusAndrade <mateus.andrade47@outlook.com>
2020-03-22 19:29:38 -0300

    feat: adding example code

--- example/src/App.js
+++ example/src/App.js
@@ -1,24 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import useViaCep from "use-via-cep";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
   const [postalCode, setPostalCode] = useState("");
-  
-  // console.log(useViaCep(postalCode));
+
+  const [loading, cep, error] = useViaCep(postalCode);
+
+  console.log("loading, cep, error", loading, cep, error);
 
   return (
     <div className="form">
       <div className="field">
-        <label>
-          CEP:
-        </label>
-        <input onChange={e => setPostalCode(e.target.value)} value={postalCode} />
+        <label>CEP:</label>
+        <input
+          onChange={e => setPostalCode(e.target.value)}
+          value={postalCode}
+        />
       </div>
+      <ul>
+        <li>Loading: {loading}</li>
+        <li>Cep: {JSON.stringify(cep)}</li>
+        <li>Error: {error}</li>
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
