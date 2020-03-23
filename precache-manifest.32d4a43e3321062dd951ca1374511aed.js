17b87f7675cbacdff4d9042187fbb3675a9b2e8d
MateusAndrade <MateusAndrade@users.noreply.github.com>
2020-03-23 17:54:37 +0000

    deploy: 50df4d492767c01e152aa364cea63a570124151d

---
+++ precache-manifest.32d4a43e3321062dd951ca1374511aed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f24afad60c368029fdc489994baebe0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c3fed80a530d2f7dd46",
+    "url": "/static/css/main.7a255f08.chunk.css"
+  },
+  {
+    "revision": "94824733f664bc4ee032",
+    "url": "/static/js/2.31d809a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.31d809a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3fed80a530d2f7dd46",
+    "url": "/static/js/main.76d733ad.chunk.js"
+  },
+  {
+    "revision": "7e65d857e43f4173d1e8",
+    "url": "/static/js/runtime-main.7813ffa7.js"
+  }
+]);
